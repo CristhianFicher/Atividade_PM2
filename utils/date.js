94bbfc12708c998b2776b74a07f76a305bab4093
Cristhian Quoghi Ficher <103082050+CristhianFicher@users.noreply.github.com>
2025-09-18 23:11:39 +0000

    Reset de arquivos

--- utils/date.js
+++
@@ -1,7 +0,0 @@
-export const formatDateTime = (date) => {
-    if (!date) return '-';
-    return new Intl.DateTimeFormat('pt-BR', {
-        dateStyle: "short",
-        timeStyle: "medium",
-    }).format(new Date(date)).replace(',', '');
-};
